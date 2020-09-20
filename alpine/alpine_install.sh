1f71839b6aacd56f5dea0067a52efbfd4472cb3d
H3nryza <20166417+h3nryza@users.noreply.github.com>
2020-09-20 04:10:47 +0200

    removed faulty lines

--- alpine/alpine_install.sh
+++ alpine/alpine_install.sh
@@ -4,51 +4,51 @@ export PACKER_VER=1.5.6
 export TF_VER=0.12.29
 
 # UPDATE
-RUN apk update && apk upgrade
+apk update && apk upgrade
 
 # COMMON
-RUN apk add bash bash-doc bash-completion curl wget zip  gzip vim openssl
+apk add bash bash-doc bash-completion curl wget zip  gzip vim openssl
 
 # ADMINISTRATION
-RUN apk add net-tools tree lynx file
+apk add net-tools tree lynx file
 
 # DEVELOPER TOOLS
-RUN apk add make git htop build-base nodejs npm
+apk add make git htop build-base nodejs npm
 
 # CLOUD
 ## AWS CLI
-RUN apk add aws-cli
+apk add aws-cli
 
 # INFRUSTRUCTURE AS CODE
 ## Packer
-RUN  wget https://releases.hashicorp.com/packer/${PACKER_VER}/packer_${PACKER_VER}_linux_amd64.zip
-RUN  unzip packer_${PACKER_VER}_linux_amd64.zip && rm -rf packer_${PACKER_VER}_linux_amd64.zip 
-RUN  mv packer /usr/local/bin
+ wget https://releases.hashicorp.com/packer/${PACKER_VER}/packer_${PACKER_VER}_linux_amd64.zip
+ unzip packer_${PACKER_VER}_linux_amd64.zip && rm -rf packer_${PACKER_VER}_linux_amd64.zip 
+ mv packer /usr/local/bin
 
 ## TERRAFORM
-RUN  wget https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_linux_amd64.zip
-RUN  unzip terraform_${TF_VER}_linux_amd64.zip && rm -rf terraform_${TF_VER}_linux_amd64.zip
-RUN  mv terraform /usr/local/bin/
+ wget https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_linux_amd64.zip
+ unzip terraform_${TF_VER}_linux_amd64.zip && rm -rf terraform_${TF_VER}_linux_amd64.zip
+ mv terraform /usr/local/bin/
 
 # CONTAINERS
 ## KUBERNETES
-RUN curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-RUN chmod +x ./kubectl
-RUN mv ./kubectl /usr/local/bin/kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
 
 ## HELM
-RUN curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-RUN chmod 700 get_helm.sh
-RUN ./get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
 ## EKSUtil
-RUN curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-RUN chmod +x /tmp/eksctl
-RUN mv /tmp/eksctl /usr/local/bin
+curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+chmod +x /tmp/eksctl
+mv /tmp/eksctl /usr/local/bin
 
 ## SNYK
-RUN npm install -g snyk
+npm install -g snyk
 
 # Create drive for user data
 #ForDriveMount
-RUN mkdir /opt/userdata -p
+mkdir /opt/userdata -p
--- ubuntu/ubu_min_installer.sh
+++ ubuntu/ubu_min_installer.sh
@@ -4,52 +4,52 @@
 export PACKER_VER=1.5.6
 export TF_VER=0.12.29
 export DEBIAN_FRONTEND=noninteractive
-
+	
 # UPDATE
-RUN apt update -y && apt upgrade -y
+apt update -y && apt upgrade -y
 
 # COMMON
-RUN apt install -y bash bash-doc bash-completion curl wget zip  gzip vim openssl
+apt install -y bash bash-doc bash-completion curl wget zip  gzip vim openssl
 
 # ADMINISTRATION
-RUN apt install -y net-tools tree lynx file
+apt install -y net-tools tree lynx file
 
 # DEVELOPER TOOLS
-RUN apt install -y make git htop make nodejs npm
+apt install -y make git htop make nodejs npm
 
 # CLOUD
 ## AWS CLI
-RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-RUN unzip awscliv2.zip
-RUN ./aws/install
-RUN rm -rf awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm -rf awscliv2.zip
 
 # INFRUSTRUCTURE AS CODE
 ## Packer
-RUN  wget https://releases.hashicorp.com/packer/${PACKER_VER}/packer_${PACKER_VER}_linux_amd64.zip
-RUN  unzip packer_${PACKER_VER}_linux_amd64.zip && rm -rf packer_${PACKER_VER}_linux_amd64.zip 
-RUN  mv packer /usr/local/bin
+ wget https://releases.hashicorp.com/packer/${PACKER_VER}/packer_${PACKER_VER}_linux_amd64.zip
+ unzip packer_${PACKER_VER}_linux_amd64.zip && rm -rf packer_${PACKER_VER}_linux_amd64.zip 
+ mv packer /usr/local/bin
 
 ## TERRAFORM
-RUN  wget https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_linux_amd64.zip
-RUN  unzip terraform_${TF_VER}_linux_amd64.zip && rm -rf terraform_${TF_VER}_linux_amd64.zip
-RUN  mv terraform /usr/local/bin/
+ wget https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_linux_amd64.zip
+ unzip terraform_${TF_VER}_linux_amd64.zip && rm -rf terraform_${TF_VER}_linux_amd64.zip
+ mv terraform /usr/local/bin/
 
 # CONTAINERS
 ## KUBERNETES
-RUN curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-RUN chmod +x ./kubectl
-RUN mv ./kubectl /usr/local/bin/kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
 
 ## HELM
-RUN curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-RUN chmod 700 get_helm.sh
-RUN ./get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
 ## EKSUtil
-RUN curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-RUN chmod +x /tmp/eksctl
-RUN mv /tmp/eksctl /usr/local/bin
+curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+chmod +x /tmp/eksctl
+mv /tmp/eksctl /usr/local/bin
 
 ## SNYK
-RUN npm install -g snyk
+npm install -g snyk
